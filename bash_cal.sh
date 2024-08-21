1119a8cdec325774b33013d5ac0e56de9e55367b
BUGHACKER <83790023+luckysitara@users.noreply.github.com>
2024-08-21 13:54:35 +0100

    Create bash_cal.sh

---
+++ bash_cal.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Function to display options
+display_menu() {
+  echo "Welcome to the Simple Bash Calculator!"
+  echo "You can perform the following operations:"
+  echo "1) Addition"
+  echo "2) Subtraction"
+  echo "3) Multiplication"
+  echo
+}
+
+# Function to add all arguments
+addition() {
+  local sum=0
+  for num in "$@"; do
+    sum=$((sum + num))
+  done
+  echo "The sum of the numbers is: $sum"
+}
+
+# Function to subtract all arguments
+subtraction() {
+  local result=$1
+  shift
+  for num in "$@"; do
+    result=$((result - num))
+  done
+  echo "The result of subtraction is: $result"
+}
+
+# Function to multiply all arguments
+multiplication() {
+  local result=1
+  for num in "$@"; do
+    result=$((result * num))
+  done
+  echo "The result of multiplication is: $result"
+}
+
+# Display the menu
+display_menu
+
+# Prompt the user to choose an operation
+read -p "Please choose an operation (1/2/3): " operation
+
+# Ask for numbers for the operation
+read -p "Enter the numbers separated by space: " -a numbers
+
+# Perform the chosen operation
+case $operation in
+  1)
+    addition "${numbers[@]}"
+    ;;
+  2)
+    subtraction "${numbers[@]}"
+    ;;
+  3)
+    multiplication "${numbers[@]}"
+    ;;
+  *)
+    echo "Invalid choice, please run the script again and choose a valid option."
+    ;;
+esac
